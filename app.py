6c99a0188b362731204e469d91e5e90f78d21aca
Sathwik Reddy <sathwikreddy@Sathwiks-Air.lan>
2025-08-14 17:13:05 -0500

    Bottswatch implemented and Cleaner Header/Badges

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import os
 from articles_website import create_app
 
 app = create_app()
 
 # ---------- Run ----------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("FLASK_RUN_PORT") or os.getenv("PORT") or "8000")
+    app.run(debug=True, port=port)
